7fec7bb6d792107cce4ac4dbeef1c3756c7feb38
Sipanale <accounts@kovale.ski>
2017-03-22 22:27:52 -0400

    initi

---
+++ static/config.js
@@ -0,0 +1,21 @@
+// ***********************************************************************************
+// ID of the Google spreadsheet
+// ***********************************************************************************
+// Make sure your spreadsheet is published (File > Publish to the web…)
+var spreadsheetId = "1HA_qMKO_Ww2-ZQvaLd93bueZIDslwFlQ5ppuGCHMVWg";
+// ***********************************************************************************
+// Your FC and IGN
+// ***********************************************************************************
+var friendCode = "3025-0363-4902";
+var inGameName = "Sipanale";
+
+// ***********************************************************************************
+// URL of your Reddit/Twitter account
+// ***********************************************************************************
+var contactUrl = "http://reddit.com/u/Sipanale";
+
+// ***********************************************************************************
+// URL of your PGL trainer icon (optional)
+// ***********************************************************************************
+// Download it from the Pokémon Global Link and re-upload to an image-sharing website
+var trainerIconUrl = "https://avatars1.githubusercontent.com/u/24725521?v=3&u=d8a1ebdb96cbe555645d86b7be6c1910552b305d&s=400";
